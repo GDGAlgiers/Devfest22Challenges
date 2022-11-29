3458154966eb7f322c9d68d809a99217e181db67
ouxs <ouxs@wanna-hack>
2022-11-29 11:31:03 +0100

    update builtins on pyjail

--- CyberSec/misc/pyjail/challenge/challenge.py
+++ CyberSec/misc/pyjail/challenge/challenge.py
@@ -5,11 +5,12 @@ import random
 whitelist = 'abcdefghijklmnopqrstuvwxyz().=|'
 
 def myflag():
-    print("DevFest22{XXXXXXXXXXXXXX}")
+    print("DevFest22{XXXXXXXXXXXXXXXXXXXXX}")
 
 if __name__ == '__main__':
     s = input("You only get once chance: ").lower()
     del __builtins__.input
+    del __builtins__.open
     if "myflag" in s or any(c not in whitelist for c in s):
         print("NAAH!")
         exit()
